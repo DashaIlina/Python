493e58a6e9b7aca545f4f0eec5d5cfcfe81be55d
Dasha <ilinadarya.mow@bk.ru>
2022-06-08 11:17:00 +0300

    1 Задача

--- seminar5HW.py
+++ seminar5HW.py
@@ -1,18 +1,33 @@
 # № 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и
 #  записать в файл многочлен степени k. *Пример: k=2 => 2*x² + 4*x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
-# def get_polynom(degree):
-#     polynom_str = ''
-#     for n in range(degree, 0, -1):
-#         a = random.randint(0, 100)
-#         if a != 0:
-#             if n == degree and n != 1:
-#                 polynom_str += str(a) + 'x^' + str(n)
-#             elif n == degree and n == 1:
-#                 polynom_str += str(a) + 'x'
-#             elif n == 1:
-#                 polynom_str += '+' + str(a) + 'x^'
-#             else:
+from random import randint
+import itertools
+
+k = randint(2, 7)
+
+def get_ratios(k):
+    ratios = [randint(0, 10) for i in range (k + 1)]
+    while ratios[0] == 0:
+        ratios[0] = randint(1, 10) 
+    return ratios
+
+def get_polynomial(k, ratios):
+    var = ['*x^']*(k-1) + ['*x']
+    polynom = [[a, b, c] for a, b, c  in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue = '') if a !=0]
+    for x in polynom:
+        x.append(' + ')
+    polynom = list(itertools.chain(*polynom))
+    polynom[-1] = ' = 0'
+    return "".join(map(str, polynom)).replace(' 1*x',' x')
+
+
+ratios = get_ratios(k)
+polynom1 = get_polynomial(k, ratios)
+print(polynom1)
+
+with open('33_Polynom.txt', 'w') as data:
+    data.write(polynom1)
 
 
 
