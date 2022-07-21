a20372b9767b5acf32ea2a2a24f20d186a40fb87
Dasha <ilinadarya.mow@bk.ru>
2022-07-21 18:00:05 +0300

    Семинар 9

--- seminar6HW.py
+++ seminar6HW.py
@@ -15,50 +15,50 @@
 # 3.Создайте программу для игры в "Крестики-нолики".
 
 
-# def krestiki():
-#     pole = [ ['-','-','-'], 
-#             ['-','-','-'], 
-#             ['-','-','-'], 
-#             ]
-#     count = 0
+def krestiki():
+    pole = [ ['-','-','-'], 
+            ['-','-','-'], 
+            ['-','-','-'], 
+            ]
+    count = 0
     
     
-#     while count < 9:
-#         if count % 2 == 0: 
-#             sign = 0
-#             player = 1
-#         else: 
-#             sign = 1
-#             player = 2
-#         step_in_row = int(input(f'{player} Игрок введите номер строки '))
-#         step_in_column = int(input(f'{player} Игрок введите номер столбца '))
+    while count < 9:
+        if count % 2 == 0: 
+            sign = 0
+            player = 1
+        else: 
+            sign = 1
+            player = 2
+        step_in_row = int(input(f'{player} Игрок введите номер строки '))
+        step_in_column = int(input(f'{player} Игрок введите номер столбца '))
                 
-#         count += 1
-#         if pole[step_in_row][step_in_column] == '-':
-#             pole[step_in_row][step_in_column] = sign
-#         else: 
-#             while pole[step_in_row][step_in_column] != '-':
-#                 print("Введите другие координаты")
-#                 step_in_row = int(input(f'{player} Игрок введите номер строки '))
-#                 step_in_column = int(input(f'{player} Игрок введите номер столбца '))
-#             pole[step_in_row][step_in_column] = sign
-#         for i in pole:print(i) 
-#         for i in range(3):
-#             if (pole[0][i]== 1 and pole[1][i]== 1 and pole[2][i] == 1) or ( pole[i][0]== 1 and pole[i][1]== 1 and pole[i][2] == 1) :
-#                 print ("выиграл 2 игрок") 
-#                 return 
-#             elif (pole[0][i]== 0 and pole[1][i]== 0 and pole[2][i] == 0) or ( pole[i][0]== 0 and pole[i][1]== 0 and pole[i][2] == 0):
-#                 print ("выиграл 1 игрок")
-#                 return
-#         if (pole[0][0]== 1 and pole[1][1]== 1 and pole[2][2] == 1) or (pole[0][2]== 1 and pole[1][1]== 1 and pole[2][0] == 1):
-#             print ("выиграл 2 игрок")
-#             return
-#         elif (pole[0][0]== 0 and pole[1][1]== 0 and pole[2][2] == 0) or (pole[0][2]== 0 and pole[1][1]== 0 and pole[2][0] == 0):
-#             print ("выиграл 1 игрок")
-#             return
-#         if count == 9:
-#             print("Победителей нет")
-# krestiki()
+        count += 1
+        if pole[step_in_row-1][step_in_column-1] == '-':
+            pole[step_in_row-1][step_in_column-1] = sign
+        else: 
+            while pole[step_in_row-1][step_in_column-1] != '-':
+                print("Введите другие координаты")
+                step_in_row = int(input(f'{player} Игрок введите номер строки '))
+                step_in_column = int(input(f'{player} Игрок введите номер столбца '))
+            pole[step_in_row-1][step_in_column-1] = sign
+        for i in pole:print(i) 
+        for i in range(3):
+            if (pole[0][i]== 1 and pole[1][i]== 1 and pole[2][i] == 1) or ( pole[i][0]== 1 and pole[i][1]== 1 and pole[i][2] == 1) :
+                print ("выиграл 2 игрок") 
+                return 
+            elif (pole[0][i]== 0 and pole[1][i]== 0 and pole[2][i] == 0) or ( pole[i][0]== 0 and pole[i][1]== 0 and pole[i][2] == 0):
+                print ("выиграл 1 игрок")
+                return
+        if (pole[0][0]== 1 and pole[1][1]== 1 and pole[2][2] == 1) or (pole[0][2]== 1 and pole[1][1]== 1 and pole[2][0] == 1):
+            print ("выиграл 2 игрок")
+            return
+        elif (pole[0][0]== 0 and pole[1][1]== 0 and pole[2][2] == 0) or (pole[0][2]== 0 and pole[1][1]== 0 and pole[2][0] == 0):
+            print ("выиграл 1 игрок")
+            return
+        if count == 9:
+            print("Победителей нет")
+krestiki()
 
 
 
---
+++ seminar9/config.py
@@ -0,0 +1,1 @@
+TOKEN = "5573504566:AAEVEXBFD6R-Add-qyqRPMbfaBFt-qccZ_c"
